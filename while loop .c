cfcebc63c4d5122b7fb2438e4da88e0c0582d2d6
JEEVANANTHAN A <106263059+jeevananthan27@users.noreply.github.com>
2023-03-07 21:38:46 +0530

    Add files via upload

---
+++ while loop .c
@@ -0,0 +1,45 @@
+//Loops
+
+
+#include <stdio.h>
+
+
+
+int main()
+
+
+{
+  
+
+  int sum=0,n,i=1;
+  
+
+  scanf("%d",&n);
+   
+
+ //while to print sum of integers
+  
+
+  while(i<=n)
+  
+
+  {
+   
+
+     sum=sum+1;
+   
+
+     i+=1;
+   
+
+ }
+  
+
+  printf("sum is %d",n);
+
+  
+
+  return 0;
+
+
+}
